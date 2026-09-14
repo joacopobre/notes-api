bd4e2c8988c7989ab7929be903b6b02120c0a32d
joaquinrodes <joaquinrodes.dev@gmail.com>
2026-09-14 14:57:33 +0100

    Add database.py: SQLAlchemy engine, session factory, Base

---
+++ database.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import  declarative_base, sessionmaker
+
+load_dotenv()
+database = os.environ.get("DATABASE_URL")
+engine = create_engine(database)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()

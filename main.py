57b0c9971beca5ec60421ea0385d6ecf7d8d8d99
joaquinrodes <joaquinrodes.dev@gmail.com>
2026-09-14 12:38:08 +0100

    Add PUT and DELETE routes to complete CRUD

--- main.py
+++ main.py
@@ -28,3 +28,21 @@ def get_note(note_id: int):
         if note['id'] == note_id:
             return note
     raise HTTPException(status_code=404, detail="Note not found")
+
+@app.put("/notes/{note_id}")
+def change_note(note_id : int, note:Note):
+    for curr_note in notes:
+        if curr_note['id'] == note_id:
+            curr_note["title"] = note.title
+            curr_note["content"] = note.content
+            return curr_note
+
+    raise HTTPException(status_code=404, detail="Note not found")
+
+@app.delete("/notes/{note_id}")
+def delete_note(note_id:int):
+    for note in notes:
+        if note['id'] == note_id:
+            notes.remove(note)
+            return {"message": "Note deleted", "id": note_id}
+    raise HTTPException(status_code=404, detail="Note not found")
